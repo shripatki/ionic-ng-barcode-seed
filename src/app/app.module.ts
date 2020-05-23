bb9838c70ac072608ee26111e7a86d6a2fc90b35
shrikant patki <shripatki9@gmail.com>
2020-05-23 18:52:01 +0530

    [SP] Done with qr code scanner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,20 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,FormsModule],
   providers: [
     StatusBar,
     SplashScreen,
+    BarcodeScanner,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {
+  BarcodeScannerOptions,
+  BarcodeScanner
+} from "@ionic-native/barcode-scanner/ngx";
 
 @Component({
   selector: 'app-home',
@@ -7,6 +11,48 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  encodedData: any;
+  scanner:boolean=true;
+    scannedData: {};
+    barcodeScannerOptions: BarcodeScannerOptions;
+  
+    constructor(private barcodeScanner: BarcodeScanner) {
+      this.encodedData = "https://www.google.com";
+      //Options
+      this.barcodeScannerOptions = {
+        showTorchButton: true,
+        showFlipCameraButton: true
+      };
+    }
+  
+    scanBar() {
+      this.barcodeScanner
+        .scan()
+        .then(barcodeData => {
+          alert("Barcode data " + JSON.stringify(barcodeData));
+          this.scannedData = barcodeData;
+        })
+        .catch(err => {
+          console.log("Error", err);
+        });
+    }
+  
+    generateBar() {
+      this.barcodeScanner
+        .encode(this.barcodeScanner.Encode.TEXT_TYPE, this.encodedData)
+        .then(
+          encodedData => {
+            console.log(encodedData);
+            this.encodedData = encodedData;
+          },
+          err => {
+            console.log("Error occured : " + err);
+          }
+        );
+    }
+
+    onSegmentChnage(event){
+      this.scanner=!this.scanner;
+    }
 
 }
